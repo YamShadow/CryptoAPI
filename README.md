eaa1410f8275cbfe4aa76fac2a15f2e6e94309b8
Mathieu YamiShadow <mathieu.nibas@gmail.com>
2018-04-24 09:15:13 +0200

    Ajout du codeIgniter + surcouche maison

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# CryptoAPI
-API dédié au Cryptomonnaie
+# Crypto API
+Projet du cours de AJAX/API/JSON.
+
+Intervenant : .
+
+Groupe :
+- Mathieu NIBAS
+- Dylan 
+
+## Getting Started
+PHP 7.x requis.  
+
+
+## Précisions sur le rendu
+
+
+## TODO
+### Pour le Front
+
+### Pour le Back
---
+++ application/helpers/crypto_helper.php
@@ -0,0 +1,29 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Methode logCrypto qui permet de crée un log
+ * * @param $message
+*/
+function logCrypto($message){
+
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) 
+		$ip = $_SERVER['HTTP_CLIENT_IP'];
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) 
+		$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else
+		$ip = $_SERVER['REMOTE_ADDR'];
+    
+    $date = new DateTime('now', new DateTimeZone('Europe/Paris'));
+
+    if(!is_dir(BASEPATH.'../logs/'))
+        mkdir(BASEPATH.'../logs/', '750');
+        
+    $log = fopen(BASEPATH.'../logs/logs-'.date('dmY').'.txt', 'a+');
+    $prepare = "[".$date->format('Y-m-d H:i:s')."] ".$ip." à l'adresse ".$_SERVER['PHP_SELF']."  - ".$message."\r\n";
+    fputs($log, $prepare);
+    fclose($log);
+}
+
+function pr($msg) {
+  echo '<pre>'.print_r($msg).'</pre>';
+}
