e9cbad0c9c9586bae0b24a22bc6aa9e0209cc057
Mathieu YamiShadow <mathieu.nibas@gmail.com>
2018-05-12 14:36:12 +0200

    Correction SQL

--- Documentation/crypto.sql
+++ Documentation/crypto.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Mer 02 Mai 2018 à 16:12
--- Version du serveur :  5.7.14
--- Version de PHP :  5.6.25
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  sam. 12 mai 2018 à 12:16
+-- Version du serveur :  5.7.19
+-- Version de PHP :  7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -26,13 +28,16 @@ SET time_zone = "+00:00";
 -- Structure de la table `echange`
 --
 
-CREATE TABLE `echange` (
-  `id` int(11) NOT NULL,
-  `last_update` date DEFAULT NULL,
+DROP TABLE IF EXISTS `echange`;
+CREATE TABLE IF NOT EXISTS `echange` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `last_update` varchar(100) COLLATE utf8_bin DEFAULT NULL,
   `1h` int(11) DEFAULT NULL,
   `24h` int(11) DEFAULT NULL,
   `7d` int(11) DEFAULT NULL,
-  `idMonnaieCrypto` int(11) DEFAULT NULL
+  `idMonnaieCrypto` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_Echange_MonnaieCrypto` (`idMonnaieCrypto`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -41,11 +46,14 @@ CREATE TABLE `echange` (
 -- Structure de la table `historique`
 --
 
-CREATE TABLE `historique` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `historique`;
+CREATE TABLE IF NOT EXISTS `historique` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `rate` varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  `last_update` date DEFAULT NULL,
-  `idMonnaieFiducaire` int(11) DEFAULT NULL
+  `last_update` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `idMonnaieFiducaire` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_Historique_MonnaieFiduciaire` (`idMonnaieFiducaire`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -54,14 +62,17 @@ CREATE TABLE `historique` (
 -- Structure de la table `historique_prix`
 --
 
-CREATE TABLE `historique_prix` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `historique_prix`;
+CREATE TABLE IF NOT EXISTS `historique_prix` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `prix` varchar(50) COLLATE utf8_bin DEFAULT NULL,
   `prix_btc` varchar(50) COLLATE utf8_bin DEFAULT NULL,
-  `24h_vol_usd` int(11) DEFAULT NULL,
+  `vol_24h_usd` varchar(150) COLLATE utf8_bin DEFAULT NULL,
   `market_cap_usd` varchar(50) COLLATE utf8_bin DEFAULT NULL,
-  `last_update` date DEFAULT NULL,
-  `idMonnaieCrypto` int(11) DEFAULT NULL
+  `last_update` varchar(100) COLLATE utf8_bin DEFAULT NULL,
+  `idMonnaieCrypto` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_HistoriquePrix_MonnaieCrypto` (`idMonnaieCrypto`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -70,11 +81,13 @@ CREATE TABLE `historique_prix` (
 -- Structure de la table `monnaie_crypto`
 --
 
-CREATE TABLE `monnaie_crypto` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `monnaie_crypto`;
+CREATE TABLE IF NOT EXISTS `monnaie_crypto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  `symbol` varchar(3) COLLATE utf8_bin DEFAULT NULL,
-  `rank` int(11) DEFAULT NULL
+  `symbol` varchar(10) COLLATE utf8_bin DEFAULT NULL,
+  `rank` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -83,48 +96,14 @@ CREATE TABLE `monnaie_crypto` (
 -- Structure de la table `monnaie_fiduciaire`
 --
 
-CREATE TABLE `monnaie_fiduciaire` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `monnaie_fiduciaire`;
+CREATE TABLE IF NOT EXISTS `monnaie_fiduciaire` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  `symbol` varchar(3) COLLATE utf8_bin DEFAULT NULL
+  `symbol` varchar(3) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `echange`
---
-ALTER TABLE `echange`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_Echange_MonnaieCrypto` (`idMonnaieCrypto`);
-
---
--- Index pour la table `historique`
---
-ALTER TABLE `historique`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_Historique_MonnaieFiduciaire` (`idMonnaieFiducaire`);
-
---
--- Index pour la table `historique_prix`
---
-ALTER TABLE `historique_prix`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_HistoriquePrix_MonnaieCrypto` (`idMonnaieCrypto`);
-
---
--- Index pour la table `monnaie_crypto`
---
-ALTER TABLE `monnaie_crypto`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `monnaie_fiduciaire`
---
-ALTER TABLE `monnaie_fiduciaire`
-  ADD PRIMARY KEY (`id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
